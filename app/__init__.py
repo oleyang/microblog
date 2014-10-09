b3ac6b4c9a027e5fd0b257282f1afbd9d494b328
oleyang <oleyang@gmail.com>
2014-10-09 11:05:45 -0400

    Hello World success!

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+# app/views.py
 from app import views
--- app/views.py
+++ app/views.py
@@ -2,8 +2,8 @@
 
 from app import app
 
+# 让/和/index都调用index函数
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return "Hello, World!"
+    return "Hello, World!(世界，我来了！)"
