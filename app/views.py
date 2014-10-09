630ff8e4d8848ad2aee4db525cd74e7365ba6eb5
oleyang <oleyang@gmail.com>
2014-10-09 12:03:08 -0400

    use template

--- app/views.py
+++ app/views.py
@@ -1,9 +1,27 @@
 #encoding=utf-8
 
+# 模板默认的目录为app/templates
+from flask import render_template
+
 from app import app
 
 # 让/和/index都调用index函数
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!(世界，我来了！)"
+    user  = {'nickname':'Oleyang'}
+    posts = [
+        {
+            'author': {'nickname':'John'},
+            'body': "beautiful day in Portland!",
+        },
+        {
+            'author': {'nickname':'Susan'},
+            'body': "The Avengers movie was so cool!",
+        },
+    ]
+    return render_template('index.html',
+                           title='Microblog Home',
+                           user=user,
+                           posts=posts
+                           )
