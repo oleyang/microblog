83c55283cd05a37dab6c9bf0920e6bce83003a4d
oleyang <oleyang@gmail.com>
2014-10-10 22:14:51 +0800

    修改了gitignore

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ microblog
 =========
 
 A python Flask Frame
+参考网址：http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+
+其中microblog/flask 变成miroblog/env
+修改代码不需要重启程序
+
+需要安装的软件
+1. Flask-WTF
+   pip install Flask-WTF
--- config.py
+++ config.py
@@ -5,7 +5,7 @@ SECRET_KEY = 'you-wil-never-guess'
 
 
 OPENID_PROVIDERS = [
-    {'name': 'Google', 'url': 'https://google.com'},
+    {'name': 'Google', 'url': 'https://www.google.com'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
